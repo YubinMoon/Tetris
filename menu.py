511e67634d2b8783fcb3b975594bb6824dc7ba78
Moon-Yubin <munu220@gmail.com>
2021-12-17 13:11:50 +0900

    first tetris

---
+++ menu.py
@@ -0,0 +1,281 @@
+import pygame
+
+from game import resource_path
+
+
+class Menu(pygame.Surface):
+  def __init__(self, size):
+    super().__init__(size, pygame.SRCALPHA)
+    self.size = size
+    self.fill(pygame.Color(0, 100, 100, 0))
+    self.group = pygame.sprite.Group()
+
+  def eventHandling(self, key):
+    List = []
+    self.group.update(key, List)
+    return List
+
+  def draw(self):
+    self.group.draw(self)
+
+
+class MainMenu(Menu):
+  def __init__(self, size):
+    super().__init__(size)
+    self.init()
+
+  def init(self):
+    self.group.add(BTN("GAME START", GAMESTART))
+    self.group.add(BTN("CONTROL", CONTROL))
+    self.group.add(BTN("HISTORY", SCORE))
+    self.group.add(BTN("EXIT", EXIT))
+    num = -1.5
+    for i in self.group:
+      i.rect = i.image.get_rect(
+          center=(self.size[0]/2, self.size[1]/2+num*200))
+      num += 1
+
+
+class PauseMenu(Menu):
+  def __init__(self, size):
+    super().__init__(size)
+    self.init()
+
+  def init(self):
+    self.group = pygame.sprite.Group()
+    self.group.add(BTN("CONTINUE", CONTINUE))
+    self.group.add(BTN("RESTAET", RESTART))
+    self.group.add(BTN("EXIT", MAINMENU))
+    num = -1
+    for i in self.group:
+      i.rect = i.image.get_rect(
+          center=(self.size[0]/2, self.size[1]/2+num*200))
+      num += 1
+
+
+class ScoreBoard(Menu):
+  def __init__(self, size: tuple, score: list):
+    super().__init__(size)
+    self.score = score[-1]
+    self.image = pygame.image.load(resource_path("src/board.png")).convert()
+    self.rect = self.image.get_rect(center=(960, 410))
+    self.initBTN()
+
+  def initBTN(self):
+    self.group = pygame.sprite.Group()
+    a = BTN("MAIN MENU", MAINMENU)
+    a.rect = a.image.get_rect(center=(self.size[0]/2, 850))
+    b = BTN("RESTART", RESTART)
+    b.rect = b.image.get_rect(center=(self.size[0]/2, 980))
+    self.group.add(a)
+    self.group.add(b)
+
+  def get_nowTime_str(self):
+    year = str(self.score["year"]).zfill(4)
+    month = str(self.score["month"]).zfill(2)
+    day = str(self.score["day"]).zfill(2)
+    hour = str(self.score["hour"]).zfill(2)
+    minute = str(self.score["minute"]).zfill(2)
+    return "%s/%s/%s-%s:%s" % (year, month, day, hour, minute)
+
+  def textSetting(self):
+    point = 100
+    surface = pygame.Surface(self.image.get_size(), flags=pygame.SRCALPHA)
+    s = getImage("SCORE", (0, 0, 0), None)
+    s_rect = s.get_rect(center=(surface.get_width()/2, point))
+    point += 100
+    score = getImage(str(self.score["score"]), (0, 0, 0), None)
+    score_rect = score.get_rect(center=(surface.get_width()/2, point))
+    point += 300
+    t = getImage("TIME", (0, 0, 0), None)
+    t_rect = t.get_rect(center=(surface.get_width()/2, point))
+    point += 100
+    time = getImage(self.get_nowTime_str(), (0, 0, 0), None, font_size=60)
+    time_rect = time.get_rect(center=(surface.get_width()/2, point))
+    surface.blit(s, (s_rect.x, s_rect.y))
+    surface.blit(score, (score_rect.x, score_rect.y))
+    surface.blit(t, (t_rect.x, t_rect.y))
+    surface.blit(time, (time_rect.x, time_rect.y))
+    return surface
+
+  def draw(self):
+    self.blit(self.image, (self.rect.x, self.rect.y))
+    self.blit(self.textSetting(), (self.rect.x, self.rect.y))
+    super().draw()
+
+
+class ScoreHistory(Menu):
+  def __init__(self, size: tuple, score: dict):
+    super().__init__(size)
+    self.score = score
+    self.header = score[0]
+    self.len = len(score) - 1
+    self.line = 7
+    self.page = 0
+    self.Lpage = self.len//self.line
+    self.init()
+
+  def init(self):
+    self.group = pygame.sprite.Group()
+    a = BTN("MAINMENU", MAINMENU)
+    a.rect = a.image.get_rect(center=(self.size[0]/2, self.get_height()-100))
+    if self.page > 0:
+      l = BTN("<", LEFT)
+      l.rect = l.image.get_rect(
+          center=(self.size[0]/2-600, self.get_height()-100))
+      self.group.add(l)
+    if self.page < self.Lpage and self.len % self.line != 0:
+      l = BTN(">", RIGHT)
+      l.rect = l.image.get_rect(
+          center=(self.size[0]/2+600, self.get_height()-100))
+      self.group.add(l)
+    self.group.add(a)
+
+  def eventHandling(self, key):
+      l = super().eventHandling(key)
+      if LEFT in l:
+        self.page -= 1
+        self.init()
+      if RIGHT in l:
+        self.page += 1
+        self.init()
+      return l
+
+  def maketop(self):
+    surface = pygame.Surface((self.size[0], 100), pygame.SRCALPHA)
+    date = getImage("DATE", (255, 255, 255), None, (200, 100))
+    time = getImage("TIME", (255, 255, 255), None, (200, 100))
+    score = getImage("SCORE", (255, 255, 255), None, (300, 100))
+    line = getImage("LINE", (255, 255, 255), None, (300, 100))
+    speed = getImage("SPEED", (255, 255, 255), None, (300, 100))
+    a = 200
+    surface.blit(date, (a, 0))
+    a += 400
+    surface.blit(time, (a, 0))
+    a += 270
+    surface.blit(score, (a, 0))
+    a += 270
+    surface.blit(line, (a, 0))
+    a += 270
+    surface.blit(speed, (a, 0))
+    return surface
+
+  def makeLine(self, score):
+    surface = pygame.Surface((self.size[0], 100), pygame.SRCALPHA)
+    year = str(score["year"]).zfill(2)
+    month = str(score["month"]).zfill(2)
+    day = str(score["day"]).zfill(2)
+    hour = str(score["hour"]).zfill(2)
+    minute = str(score["minute"]).zfill(2)
+    d = "%s/%s/%s" % (year,month,day)
+    t = "%s:%s" % (hour,minute)
+    d = getImage(d, (255, 255, 255), None, (400, 100))
+    t = getImage(t, (255, 255, 255), None, (300, 100))
+    S = getImage(str(score["score"]), (255, 255, 255), None, (200, 100))
+    line = getImage(str(score["ereaseBlock"]), (255, 255, 255), None, (200, 100))
+    speed = getImage(str(score["speed"]), (255, 255, 255), None, (200, 100))
+    a = 100
+    surface.blit(d, (a, 0))
+    a += 450
+    surface.blit(t, (a, 0))
+    a += 370
+    surface.blit(S, (a, 0))
+    a += 270
+    surface.blit(line, (a, 0))
+    a += 270
+    surface.blit(speed, (a, 0))
+    return surface
+
+  def drawList(self):
+    if self.page < self.Lpage:
+      for i in range(self.line):
+        self.blit(self.makeLine(
+            self.score[-(self.line*self.page+i+1)]), (0, 200+i*100))
+    else:
+      for i in range(self.len % self.line):
+        self.blit(self.makeLine(
+            self.score[-(self.line*self.page+i+1)]), (0, 200+i*100))
+
+  def draw(self):
+    version = getImage(
+        "version:"+self.header["version"], (255, 255, 255), None, (100, 30), 20)
+    self.fill(pygame.Color(0, 0, 0, 0))
+    self.blit(version, (30, 30))
+    self.blit(self.maketop(), (0, 100))
+    # self.blit(self.makeLine(self.score[1]),(0,200))
+    self.drawList()
+    super().draw()
+    
+    
+class Control(Menu):
+  def __init__(self, size):
+    super().__init__(size)
+    self.init()
+
+  def init(self):
+    self.group = pygame.sprite.Group()
+    a = BTN("MAINMENU", MAINMENU)
+    a.rect = a.image.get_rect(center=(self.get_width()/2,self.get_height()-100))
+    self.group.add(a)
+    self.consurface = pygame.Surface(self.size,pygame.SRCALPHA)
+    controlList = []
+    controlList.append("RIGHT: move right")
+    controlList.append("LEFT: move left")
+    controlList.append("UP: trun right")
+    controlList.append("DOWN: soft drop")
+    controlList.append("SPACE: hard drop")
+    controlList.append("Z: hold")
+    controlList.append("F: full screen toggle")
+    controlList.append("M: bgm mute toggle")
+    controlList.append("ESC: menu & quit")
+    for n, i in enumerate(controlList):
+      img = getImage(i,(255,255,255),None,(self.get_width(),100),75)
+      self.consurface.blit(img,(0,n*90+70))
+  def draw(self):
+    self.blit(self.consurface,(0,0))
+    super().draw()
+      
+
+class BTN(pygame.sprite.Sprite):
+  def __init__(self, text: str, clicked):
+    super().__init__()
+    self.text = text
+    self.clicked = clicked
+    self.normal = getImage(self.text, (255, 255, 255), (0, 0, 0))
+    self.toggle = getImage(self.text, (0, 0, 0), (255, 255, 255))
+    # self.image = getImage(self.text, (0, 255, 0), (255, 255, 255))
+    self.image = self.normal
+
+  def update(self, key: dict, List: list):
+    pos = key["pos"]
+    if self.rect.collidepoint(pos[0]*key["ratio_x"], pos[1]*key["ratio_x"]):
+      if key["pressed"]:
+        self.image = getImage(self.text, (0, 255, 0), (255, 255, 255))
+      else:
+        self.image = self.toggle
+      if key["clicked"]:
+        List.append(self.clicked)
+    else:
+      self.image = self.normal
+
+
+def getImage(text, color, background, size: tuple = (500, 100), font_size=80):
+  font = pygame.font.SysFont("Arial", font_size)
+  textSurf = font.render(text, 1, color)
+  image = pygame.Surface(size, pygame.SRCALPHA)
+  if background:
+    image.fill(background)
+  trect = textSurf.get_rect(center=(size[0]/2, size[1]/2))
+  image.blit(textSurf, trect)
+  return image
+
+
+GAMESTART = 200
+RESTART = 301
+CONTROL = 201
+CONTINUE = 202
+MAINMENU = 300
+EXIT = 203
+SCORE = 4303
+RIGHT = 21
+LEFT = 22
--- tetris.py
+++ tetris.py
@@ -1,242 +1,331 @@
 import os
-import math
+import sys
+import time
+import ctypes
+import pickle
+import threading
+
+from pygame._sdl2.video import Window
 import pygame
+from menu import Control, MainMenu, ScoreBoard, ScoreHistory, Control
+from menu import PauseMenu
+from menu import GAMESTART, CONTINUE, CONTROL, SCORE, EXIT, MAINMENU, RESTART
+from datetime import datetime
+from game import Container
+from game import GAMEOVER
+from pygame.constants import DOUBLEBUF, NOFRAME
+
+ctypes.windll.user32.SetProcessDPIAware()  # 윈도우 화면 배율 무시
 
 
-class mainDisplay():
-    def __init__(self):
-        self.background = pygame.image.load(
-            os.path.join(current_path, "container.png"))
-        self.rect = self.background.get_rect(
-            center=(screen_width//2, screen_height//2))
-        self.map = []
-        for i in range(20):
-            self.map.append(list(".........."))
-        self.currentBlock = None
-        self.blockSize = (self.rect.right - self.rect.left)//10
-        self.currentTime = 0
-        self.floorTime = 0
-        self.isFloor = False
-
-    def makeCurrentBlock(self):
-        if not self.currentBlock:
-            self.currentBlock = tetromino(self, "T")
-            self.currentBlock.makeBlock()
-
-    def moveCurrentBlock(self):
-        global toRight, toLeft, toUp
-        if toRight:
-            self.currentBlock.moveRight()
-            toRight = False
-        if toLeft:
-            self.currentBlock.moveLeft()
-            toLeft = False
-        if toUp:
-            self.currentBlock.rotateRight()
-            self.floorTime = 0
-            self.isFloor = False
-            toUp = False
-
-    def blockDown(self, dt):
-        self.currentTime += dt
-        self.floorTime += dt
-        if self.currentTime > BLOCK_TICK:
-            self.currentTime = 0
-            self.isFloor = self.currentBlock.down()
-        if self.isFloor and self.floorTime > MAX_FLOOR_TICK:
-            self.floorTime = 0
-            self.isFloor = False
-            self.currentBlock.setMap()
-            self.currentBlock = tetromino(self, "I")
-
-    def drawMap(self):
-        for y, i in enumerate(self.map):
-            for x, type in enumerate(i):
-                if type == ".":
-                    continue
-                screen.blit(
-                    blockImage[0], (self.rect.left+x*self.blockSize, self.rect.top+y*self.blockSize))
-
-    def draw(self):
-        screen.blit(self.background, self.rect)
-        self.drawMap()
-        self.currentBlock.draw()
+def resource_path(relative_path):  # 파일 절대경로 추적
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 
-class block(pygame.sprite.Sprite):
-    def __init__(self, type, image):
-        super().__init__()
-        self.type = type
-        self.block = image
-
-
-class tetromino():
-    def __init__(self, container, type):
-        self.container = container
-        self.type = type
-        self.blockList = BLOCK_LIST[type]
-        self.rotateValue = ROTATE_VALUE[type]
-        self.map = []
-        for i in range(20):
-            self.map.append(list(".........."))
-        self.toX = 0
-        self.toY = 0
-        self.clearMap()
-
-    def clearMap(self):
-        self.map.clear()
-        for i in range(20):
-            self.map.append(list(".........."))
-
-    def moveRight(self):
-        self.toX += 1
-
-    def moveLeft(self):
-        self.toX -= 1
-
-    def rotateRight(self):
-        mainMap = self.container.map
-        temp = [(i[0]-self.rotateValue[0], i[1]-self.rotateValue[1])
-                for i in self.blockList]
-        temp = [(-i[1], i[0]) for i in temp]
-        temp = [(int(i[0]+self.rotateValue[1]), int(i[1]+self.rotateValue[0]))
-                for i in temp]
-        self.blockList = temp
-        self.availabilityCheck()
-
-    def makeBlock(self):
-        for i in self.blockList:
-            self.map[i[1]+self.toY][i[0]+self.toX] = self.type
-
-    def down(self):
-        mainMap = self.container.map
-        for i in self.blockList:
-            if i[1]+self.toY == MAP_HEIGHT-1:
-                return True
-            elif mainMap[i[1]+self.toY+1][i[0]+self.toX] != ".":
-                return True
-        self.toY += 1
-
-    def wallCollision(self):
-        for i in self.blockList:
-            if i[0] + self.toX < 0:
-                self.toX += -(i[0] + self.toX)
-            elif i[0] + self.toX > MAP_WIDTH - 1:
-                self.toX -= (i[0] + self.toX) - (MAP_WIDTH - 1)
-            if i[1] + self.toY > MAP_HEIGHT - 1:
-                self.toY -= (i[1] + self.toY) - (MAP_HEIGHT - 1)
-
-    def availabilityCheck(self):
-        for i in self.blockList:
-            if i[0] + self.toX < 0:
-                self.toX += -(i[0] + self.toX)
-            elif i[0] + self.toX > MAP_WIDTH - 1:
-                self.toX -= (i[0] + self.toX) - (MAP_WIDTH - 1)
-            if i[1] + self.toY > MAP_HEIGHT - 1:
-                self.toY -= (i[1] + self.toY) - (MAP_HEIGHT - 1)
-
-    def setMap(self):
-        mainMap = self.container.map
-        for i in self.blockList:
-            mainMap[i[1]+self.toY][i[0]+self.toX] = self.type
-
-    def draw(self):
-        self.clearMap()
-        self.availabilityCheck()
-        for i in self.blockList:
-            self.map[i[1]+self.toY][i[0]+self.toX] = self.type
-        for y, i in enumerate(self.map):
-            for x, type in enumerate(i):
-                if type == ".":
-                    continue
-                screen.blit(blockImage[0], (self.container.rect.left+x*self.container.blockSize,
-                            self.container.rect.top+y*self.container.blockSize))
-
-
-#초기화
+# 초기화
 pygame.init()
 
-#화면 크기 설정
-screen_width = 1280
-screen_height = 720
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-#화면 타이틀 설정
+# 화면 크기 설정
+displayInfo = pygame.display.Info()
+# SCREEN_WIDTH = displayInfo.current_w
+# SCREEN_HEIGHT = displayInfo.current_h
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_SIZE = pygame.display.list_modes()[0]
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+SCREEN = pygame.display.set_mode(SCREEN_SIZE, DOUBLEBUF | NOFRAME)
+WINDOW = pygame.Surface((1920, 1080), pygame.SRCALPHA)
+FPS = 30
+HEADER = {
+    "name": "TETRIS",
+    "maker": "terry",
+    "version": "10.3",
+}
+# 화면 타이틀 설정
 pygame.display.set_caption("TETRIS")
 
-#FPS
-clock = pygame.time.Clock()
 
-#1. 사용자 게임 초기화
-BLOCK_TICK = 200
-MAX_FLOOR_TICK = 500
-MAP_WIDTH = 10
-MAP_HEIGHT = 20
-BLOCK_LIST = {
-    "I": [(0, 2), (1, 2), (2, 2), (3, 2)],
-    "O": [(0, 0), (1, 0), (0, 1), (1, 1)],
-    "Z": [(0, 0), (1, 0), (1, 1), (2, 1)],
-    "S": [(1, 0), (2, 0), (0, 1), (1, 1)],
-    "J": [(0, 0), (0, 1), (1, 1), (2, 1)],
-    "L": [(2, 0), (0, 1), (1, 1), (2, 1)],
-    "T": [(1, 0), (0, 1), (1, 1), (2, 1)]
-}
-ROTATE_VALUE = {
-    "I": (1.5, 1.5),
-    "O": (0.5, 0.5),
-    "Z": (1, 1),
-    "S": (1, 1),
-    "J": (1, 1),
-    "L": (1, 1),
-    "T": (1, 1)
-}
-current_path = os.path.dirname(__file__)
-background = pygame.image.load(os.path.join(current_path, "background.png"))
-firstMap = mainDisplay()
-spriteGroup = pygame.sprite.Group()
-blockImage = [pygame.image.load(os.path.join(current_path, "red.png"))]
-toRight = False
-toLeft = False
-toUp = False
-toDown = False
+# FPS
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("arial", 80, True)
+background = pygame.image.load(resource_path("src/background.png")).convert()
+icon = pygame.image.load(resource_path("src/icon.png"))
+bgm_game = pygame.mixer.Sound(resource_path("src/bgm/game.wav"))
+bgm_game.set_volume(0.1)
+bgm_menu = pygame.mixer.Sound(resource_path("src/bgm/menu.wav"))
+bgm_menu.set_volume(0.3)
+pygame.display.set_icon(icon)
+userdir = os.path.join(os.path.expandvars(r'%LOCALAPPDATA%'), "Tetris")
+scoredir = os.path.join(userdir, "score.bin")
+print(userdir)
+key = {}
+
 
 running = True
-while running:
-    dt = clock.tick(60)
+totalScore = []
+window = Window.from_display_module()
 
-    #2. 이벤트 처리
+
+def get_totalScore():
+    global totalScore
+    if not os.path.exists(userdir):
+        os.makedirs(userdir)
+    if os.path.exists(scoredir):
+        totalScore = pickle.load(open(scoredir, "rb"))
+    else:
+        totalScore.append(HEADER)
+
+
+threading.Thread(target=get_totalScore).start()
+
+
+def savingScore():
+    pickle.dump(totalScore, open(scoredir, "wb"))
+
+
+def saveScore(score: dict):
+    global totalScore
+    now = datetime.now()
+    temp = {
+        "year": now.year,
+        "month": now.month,
+        "day": now.day,
+        "hour": now.hour,
+        "minute": now.minute,
+        "second": now.second,
+        "score": score["score"],
+        "speed": score["speed"],
+        "ereaseBlock": score["ereaseBlock"]
+    }
+    totalScore.append(temp)
+
+
+def eventSet():
+    key[pygame.K_RIGHT] = False
+    key[pygame.K_LEFT] = False
+    key[pygame.K_UP] = False
+    key[pygame.K_DOWN] = False
+    key[pygame.K_SPACE] = False
+    key[pygame.K_z] = False
+    key[pygame.K_ESCAPE] = False
+    key["clicked"] = False
+    key["pressed"] = False
+    key["ratio_x"] = 1
+    key["ratio_y"] = 1
+    key["pos"] = (0, 0)
+
+
+eventSet()
+
+
+def eventHandler():
+    global running, SCREEN, SCREEN_SIZE
+    key["clicked"] = False
+    key[pygame.K_ESCAPE] = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                toRight = True
-            if event.key == pygame.K_LEFT:
-                toLeft = True
-            if event.key == pygame.K_UP:
-                toUp = True
-            if event.key == pygame.K_DOWN:
-                toDOWN = True
+            key[event.key] = True
+            if event.key == pygame.K_f:
+                if SCREEN.get_flags() & NOFRAME:
+                    size = (1600, 900)
+                    SCREEN = pygame.display.set_mode(size, DOUBLEBUF)
+                    key["ratio_x"] = SCREEN_WIDTH/1600
+                    key["ratio_y"] = SCREEN_HEIGHT/900
+                    window.position = ((1920-1600)/2, (1080-900)/2)
+                else:
+                    SCREEN = pygame.display.set_mode(
+                        SCREEN_SIZE, DOUBLEBUF | NOFRAME)
+                    key["ratio_x"] = 1
+                    key["ratio_y"] = 1
+                    window.position = (0, 0)
+            if event.key == pygame.K_m:
+                if bgm_game.get_volume() == 0:
+                    bgm_game.set_volume(0.1)
+                    bgm_menu.set_volume(0.3)
+                else:
+                    bgm_game.set_volume(0)
+                    bgm_menu.set_volume(0)
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                toRight = False
-            if event.key == pygame.K_LEFT:
-                toLeft = False
-            if event.key == pygame.K_UP:
-                toUp = False
-            if event.key == pygame.K_DOWN:
-                toDOWN = False
+            key[event.key] = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            key["clicked"] = True
+        key["pressed"] = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            key["pressed"] = True
 
-    #3. 게임 캐릭터 위치 정의
-    firstMap.makeCurrentBlock()
-    #4. 충돌처리
-    firstMap.moveCurrentBlock()
-    firstMap.blockDown(dt)
+    key["pos"] = pygame.mouse.get_pos()
 
-    #5.화면에 그리기
-    screen.blit(background, (0, 0))
-    firstMap.draw()
-    #spriteGroup.draw(screen)
-    pygame.display.update()
+
+def menu():
+    global running
+    mainMenu = MainMenu(SCREEN_SIZE)
+    bgm_game.fadeout(500)
+    bgm_menu.fadeout(500)
+    bgm_menu.play(-1, fade_ms=500)
+    while running:
+        dt = clock.tick(FPS)
+        eventHandler()
+        if key[pygame.K_ESCAPE]:
+            running = False
+            sys.exit()
+        for i in mainMenu.eventHandling(key):
+            if i == GAMESTART:
+                bgm_menu.fadeout(500)
+                playGame()
+            elif i == CONTROL:
+                control()
+            elif i == SCORE:
+                scoreHistory()
+            elif i == EXIT:
+                running = False
+                bgm_menu.fadeout(500)
+                sys.exit()
+        mainMenu.draw()
+        WINDOW.blit(background, (0, 0))
+        WINDOW.blit(mainMenu, (0, 0))
+        SCREEN.blit(pygame.transform.scale(
+            WINDOW, (SCREEN.get_rect().width, SCREEN.get_rect().height)), (0, 0))
+        pygame.display.update()
+
+
+def playGame():
+    global running
+    mainMap = Container((680, 940))
+    bgm_game.fadeout(500)
+    bgm_game.play(-1, fade_ms=500)
+    while running:
+        dt = clock.tick(FPS)  # MAX FPS
+        eventHandler()
+        if key[pygame.K_ESCAPE]:
+            pygame.mixer.pause()
+            pause(mainMap)
+            pygame.mixer.unpause()
+        # 4. 충돌처리
+        for i in mainMap.update(dt, key):
+            if i == GAMEOVER:
+                bgm_game.fadeout(500)
+                saveScore(mainMap.get_status())
+                scoreBoard(mainMap)
+        mainMap_rect = mainMap.get_rect(
+            center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        # 화면에 그리기
+        WINDOW.blit(background, (0, 0))
+        mainMap.draw()
+        WINDOW.blit(mainMap, (mainMap_rect.x, mainMap_rect.y))
+        SCREEN.blit(pygame.transform.scale(
+            WINDOW, (SCREEN.get_rect().width, SCREEN.get_rect().height)), (0, 0))
+        pygame.display.update()
+
+
+def pause(mainMap: Container):
+    global running
+    pause = PauseMenu(SCREEN_SIZE)
+    while running:
+        dt = clock.tick(FPS)
+        eventHandler()
+        if key[pygame.K_ESCAPE]:
+            key[pygame.K_ESCAPE] = False
+            return
+        for i in pause.eventHandling(key):
+            if i == CONTINUE:
+                return
+            elif i == MAINMENU:
+                menu()
+            elif i == RESTART:
+                playGame()
+        mainMap_rect = mainMap.get_rect(
+            center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        pause.draw()
+        a = pygame.Surface(SCREEN_SIZE, pygame.SRCALPHA)
+        a.fill(pygame.Color(0, 0, 0, 60))
+        WINDOW.blit(background, (0, 0))
+        WINDOW.blit(mainMap, (mainMap_rect.x, mainMap_rect.y))
+        WINDOW.blit(a, (0, 0))
+        WINDOW.blit(pygame.transform.smoothscale(
+            pygame.transform.smoothscale(WINDOW, (160, 90)), SCREEN_SIZE), (0, 0))
+        WINDOW.blit(pause, (0, 0))
+        SCREEN.blit(pygame.transform.scale(
+            WINDOW, (SCREEN.get_rect().width, SCREEN.get_rect().height)), (0, 0))
+        pygame.display.update()
+
+
+def scoreBoard(mainMap: Container):
+    board = ScoreBoard(SCREEN_SIZE, totalScore)
+    threading.Thread(target=savingScore).start()
+    while running:
+        dt = clock.tick(FPS)
+        eventHandler()
+        if key[pygame.K_ESCAPE]:
+            key[pygame.K_ESCAPE] = False
+            menu()
+        for i in board.eventHandling(key):
+            if i == MAINMENU:
+                menu()
+            elif i == RESTART:
+                playGame()
+        mainMap_rect = mainMap.get_rect(
+            center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+        board.draw()
+        a = pygame.Surface(SCREEN_SIZE, pygame.SRCALPHA)
+        a.fill(pygame.Color(0, 0, 0, 60))
+        WINDOW.blit(background, (0, 0))
+        WINDOW.blit(mainMap, (mainMap_rect.x, mainMap_rect.y))
+        WINDOW.blit(pygame.transform.smoothscale(
+            pygame.transform.smoothscale(WINDOW, (160, 90)), SCREEN_SIZE), (0, 0))
+        WINDOW.blit(a, (0, 0))
+        WINDOW.blit(board, (0, 0))
+        SCREEN.blit(pygame.transform.scale(
+            WINDOW, (SCREEN.get_rect().width, SCREEN.get_rect().height)), (0, 0))
+        pygame.display.update()
+
+
+def scoreHistory():
+    page = ScoreHistory(SCREEN_SIZE, totalScore)
+    while running:
+        dt = clock.tick(FPS)
+        eventHandler()
+        if key[pygame.K_ESCAPE]:
+            key[pygame.K_ESCAPE] = False
+            menu()
+        for i in page.eventHandling(key):
+            if i == MAINMENU:
+                menu()
+        page.draw()
+        WINDOW.blit(background, (0, 0))
+        WINDOW.blit(page, (0, 0))
+        SCREEN.blit(pygame.transform.scale(
+            WINDOW, (SCREEN.get_rect().width, SCREEN.get_rect().height)), (0, 0))
+        pygame.display.update()
+
+
+def control():
+    con = Control(SCREEN_SIZE)
+    while running:
+        dt = clock.tick(FPS)
+        eventHandler()
+        if key[pygame.K_ESCAPE]:
+            key[pygame.K_ESCAPE] = False
+            menu()
+        for i in con.eventHandling(key):
+            if i == MAINMENU:
+                menu()
+        con.draw()
+        WINDOW.blit(background, (0, 0))
+        WINDOW.blit(con, (0, 0))
+        SCREEN.blit(pygame.transform.scale(
+            WINDOW, (SCREEN.get_rect().width, SCREEN.get_rect().height)), (0, 0))
+        pygame.display.update()
+
+
+while running:
+    menu()
+    playGame()
 
 pygame.quit()
